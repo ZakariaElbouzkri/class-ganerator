fc784aae3ebec7a1aab408d1b27561532d8c481c
Zakaria El Bouzkri <zel-bouz@e3r5p4.1337.ma>
2023-09-14 01:33:22 +0100

    script

---
+++ script.py
@@ -0,0 +1,83 @@
+import sys
+
+def	wrieTemp(temp, fileName):
+	with open(fileName, 'w') as f:
+		print(temp, file=f)
+		f.close()
+
+if (len(sys.argv) < 1):
+	print("Pass className as args", file=sys.stderr)
+	sys.exit(1)
+className = sys.argv[1]
+
+
+# Makfile template
+
+Makefile = f"""NAME = {className}
+CC   = g++ -Wall -Wextra -Werror -std=c++98
+SRC  = main.cpp {className}.cpp
+INC  = {className}.hpp
+RM   = rm -f
+OBJ  = $(SRC:.cpp=.o)\n
+all: $(NAME)\n
+$(NAME): $(OBJ)
+	$(CC) $^ -o $@\n
+%.o: %.cpp $(INC)
+	$(CC) -c $< -o $@\n
+clean:
+	$(RM) $(OBJ)\n
+fclean: clean
+	$(RM) $(NAME)\n
+re: fclean all
+"""
+
+
+# className.cpp template
+
+cppFile = f"""#include "{className}.hpp"\n\n
+{className}::{className}( void ){{
+	std::cout << "default constructor called\\n";
+}}\n
+{className}::~{className}( void ){{
+	std::cout << "destructor called\\n";
+}}\n
+{className}::{className}( {className} const& rhs ){{
+	std::cout << "the copy constructor called\\n";
+	if (this != &rhs){{\n
+	}}
+}}\n
+{className}&	{className}::operator=( const {className}& rhs ){{
+	std::cout << "the copy asignment operator overload called\\n";
+	if (this != &rhs){{\n
+	}}
+	return (*this);
+}}\n
+"""
+
+# className.hpp template
+
+hppFile = f"""#ifndef _{className}_HPP_
+#define _{className}_HPP_\n
+#include <iostream>\n
+class	{className}{{\n
+	private:\n
+	public:
+		{className}( void );
+		~{className}( void );
+		{className}( {className} const& rhs );
+		{className}&	operator=( const {className}& rhs );
+}};\n
+#endif
+"""
+
+mainFile = f"""#include "{className}.hpp"\n\n
+int	main(){{
+	std::cout << "hello world\\n";
+	return (0);
+}}\n
+"""
+
+wrieTemp(Makefile, "Makefile")
+wrieTemp(mainFile, "main.cpp")
+wrieTemp(cppFile, f"{className}.cpp")
+wrieTemp(hppFile, f"{className}.hpp")

286e57e785b039fc7aaef57c53ea5fe3b210d048
Zakaria El bouzkri <elbouzkri9@gmail.com>
2023-09-22 08:07:18 +0100

    some updates

--- script.py
+++ script.py
@@ -1,22 +1,81 @@
+
 import sys
 
-def	wrieTemp(temp, fileName):
+def	createTemplate(temp, fileName):
 	with open(fileName, 'w') as f:
 		print(temp, file=f)
 		f.close()
 
-if (len(sys.argv) < 1):
-	print("Pass className as args", file=sys.stderr)
+if (len(sys.argv) < 2):
+	print("Pass cls as args", file=sys.stderr)
 	sys.exit(1)
-className = sys.argv[1]
 
+classes = sys.argv[1:]
+mainClass = classes[0]
+
+for cls in classes:
+	# cls.cpp template
+	cppFile = f"""#include "{cls}.hpp"\n\n
+{cls}::{cls}( void )
+{{
+	std::cout << "Default constructor called\\n";
+}}\n
+{cls}::~{cls}( void )
+{{
+	std::cout << "Destructor called\\n";
+}}\n
+{cls}::{cls}( {cls} const& rhs )
+{{
+	std::cout << "Copy constructor called\\n";
+	*this = rhs;
+}}\n
+{cls}&	{cls}::operator=( const {cls}& rhs )
+{{
+	std::cout << "Copy assignment operator called\\n";
+	if (this != &rhs){{\n
+	}}
+	return (*this);
+}}"""
+
+	# cls.hpp template
+
+	hppFile = f"""#ifndef _{cls}_HPP_
+#define _{cls}_HPP_\n
+#include <iostream>\n
+class	{cls}
+{{\n
+	private:\n
+	public:
+		{cls}( void );
+		~{cls}( void );
+		{cls}( {cls} const& rhs );
+		{cls}&	operator=( const {cls}& rhs );
+}};\n
+#endif"""
+
+	createTemplate(cppFile, f"{cls}.cpp")
+	createTemplate(hppFile, f"{cls}.hpp")
+
+includes = [f'{cls}.hpp' for cls in classes]
+
+files = [f'{cls}.cpp' for cls in classes]
+
+mainFile = f"""#include "{mainClass}.hpp"\n\n
+int	main()
+{{
+	std::cout << "hello world\\n";
+	return (0);
+}}"""
 
 # Makfile template
 
-Makefile = f"""NAME = {className}
+
+# print(includes)
+
+Makefile = f"""NAME = {mainClass}
 CC   = g++ -Wall -Wextra -Werror -std=c++98
-SRC  = main.cpp {className}.cpp
-INC  = {className}.hpp
+SRC  = main.cpp {" ".join(files)}
+INC  = {" ".join(includes)}
 RM   = rm -f
 OBJ  = $(SRC:.cpp=.o)\n
 all: $(NAME)\n
@@ -30,48 +89,6 @@ fclean: clean
 	$(RM) $(NAME)\n
 re: fclean all"""
 
-# className.cpp template
-
-cppFile = f"""#include "{className}.hpp"\n\n
-{className}::{className}( void ){{
-	std::cout << "Default constructor called\\n";
-}}\n
-{className}::~{className}( void ){{
-	std::cout << "Destructor called\\n";
-}}\n
-{className}::{className}( {className} const& rhs ){{
-	std::cout << "Copy constructor called\\n";
-	*this = rhs;
-}}\n
-{className}&	{className}::operator=( const {className}& rhs ){{
-	std::cout << "Copy assignment operator called\\n";
-	if (this != &rhs){{\n
-	}}
-	return (*this);
-}}"""
-
-# className.hpp template
-
-hppFile = f"""#ifndef _{className}_HPP_
-#define _{className}_HPP_\n
-#include <iostream>\n
-class	{className}{{\n
-	private:\n
-	public:
-		{className}( void );
-		~{className}( void );
-		{className}( {className} const& rhs );
-		{className}&	operator=( const {className}& rhs );
-}};\n
-#endif"""
-
-mainFile = f"""#include "{className}.hpp"\n\n
-int	main(){{
-	std::cout << "hello world\\n";
-	return (0);
-}}"""
-
-wrieTemp(Makefile, "Makefile")
-wrieTemp(mainFile, "main.cpp")
-wrieTemp(cppFile, f"{className}.cpp")
-wrieTemp(hppFile, f"{className}.hpp")
+# print(Makefile)
+createTemplate(Makefile, "Makefile")
+createTemplate(mainFile, "main.cpp")
